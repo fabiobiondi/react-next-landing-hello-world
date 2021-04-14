40ef2b633e763ddd36bd4b2e0423089c0e7205a9
Fabio Biondi <info@fabiobiondi.com>
2021-04-14 16:39:10 +0200

    Add XYZ to Giulia

--- pages/posts/curtis.tsx
+++ pages/posts/curtis.tsx
@@ -3,7 +3,7 @@ import { CurtisProfile } from '../../components/curtis/CurtisProfile';
 export default function Curtis(props) {
   return (
     <div>
-      <CurtisProfile title="Giulia" />
+      <CurtisProfile title="Giulia XYZ" />
     </div>
   )
 }
